856687a2f55630db9f08a313ef38b3011b178a27
Sergey Makarov <bigmak.87@mail.ru>
2015-08-14 16:06:55 +0300

    Object.create

---
+++ core/objects/objectCreate.js
@@ -0,0 +1,9 @@
+/**
+ * ECMAScript5 defines a method, Object.create, that creates a new object, using its first argument as the prototype of that object.
+ * Optional second argument describes the properties of the new object.
+ * This method is a static function
+ **/
+var test = Object.create({test: 'test'});
+var nilObj = Object.create(null); //nilObj inherits no methods and props
+//If you want create an ordinary empty  object:
+var emptyObj = Object.create(Object.prototype);
