c012e42ad700bfa596c45172a21206a4fda98bb4
Sergey Makarov <bigmak.87@mail.ru>
2015-08-18 17:58:08 +0300

    update equality info

--- core/conversions/equality.js
+++ core/conversions/equality.js
@@ -1,4 +1,6 @@
-null == undefined; // true,
+null == undefined; // Null and Undefined Types are strictly equal to themselves and abstractly equal to each other.
+null > undefined; //false, because null converts to 0 undefined converts to NaN
+null >= undefined; //false, because null converts to 0 undefined converts to NaN
 "0" == 0; //true, because   string converts to number before comparing
 0 == false; //true boolean converts to number before comparing
 "0" == false; //true both converts to number before comparing
